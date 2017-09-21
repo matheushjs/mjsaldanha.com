ffddf39167e010903ce9dde790703230bea2560d
Matheus Saldanha <matheus.saldanha@usp.br>
2017-09-20 21:03:47 -0300

    Change directory organization

--- index.js
+++ index.js
@@ -3,16 +3,11 @@ const path = require('path');
 
 const app = express();
 
-// Probably selects what directory contains static files that can be cached
-app.use(express.static(__dirname));
+// Serves files in the given folder, as it was an absolute path.
+// so /index.html leads to public/index.html
+app.use(express.static(path.join(__dirname, 'public')));
 
-
-app.get('*', (req, res) => {
-  res.send("<h1>Hello World</h1>");
-});
-
-
+// Begin serving
 const port = process.env.PORT || 5000;
 app.listen(port);
-
 console.log(`Server listening on ${port}`);
