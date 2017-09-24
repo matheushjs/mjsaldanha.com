d9cfcc79a631a85eb09e662aac28e1428b0b31c1
Matheus Saldanha <matheus.saldanha@usp.br>
2017-09-23 23:17:46 -0300

    Fix static folder

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const app = express();
 
 // Serves files in the given folder, as it was an absolute path.
 // so /index.html leads to public/index.html
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client')));
+
+// We can add multiple folders, express will look for due files sequentially
+// app.use(express.static(path.join(__dirname, 'static')));
 
 // Begin serving
 const port = process.env.PORT || 5000;
