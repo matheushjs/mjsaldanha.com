ba42c56b119627ed7953a00cc7768b4e3e1d75ca
Matheus Saldanha <matheus.saldanha@usp.br>
2018-02-14 17:45:40 -0200

    Add hashing to user management

--- src/authentication.js
+++ src/authentication.js
@@ -1,10 +1,17 @@
+const crypto = require('crypto');
+const keyBytes = 16; // Size of the secret keys to be used with Hmac
 
-dummy_users = { }
+var dummy_users = { }
 
 function authenticate(user, pass){
-  if(dummy_users[user] && dummy_users[user] === pass){
+  if(!dummy_users[user]) return false; // User does not exist
+  
+  const secret = dummy_users[user].substr(0, keyBytes*2);
+  const hash = crypto.createHmac('sha256', secret).update(pass).digest('hex');
+
+  if(dummy_users[user] === secret + hash)
     return true;
-  } else return false;
+  else return false;
 }
 
 function lookup(user){
@@ -12,7 +19,9 @@ function lookup(user){
 }
 
 function add_user(user, pass, name){
-  dummy_users[user] = pass;
+  const secret = crypto.randomBytes(keyBytes).toString('hex');
+  const hash = crypto.createHmac('sha256', secret).update(pass).digest('hex');
+  dummy_users[user] = secret + hash;
 }
 
 function sign_up(user, pass, name){
