5c9b578e5034dfe54a154b82449ecf22682999a5
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-07-11 23:34:57 -0300

    Fix indentation

--- server/view/renderer.js
+++ server/view/renderer.js
@@ -1,49 +1,49 @@
 
 class Renderer {
-    constructor(callname = undefined, specialUser = false){
-        this.callname = callname;
-        this.specialUser = specialUser;
-    }
-    
-    messagePage(res, message){
-        res.render("message_page", {
-            callname: this.callname,
-            specialUser: this.specialUser,
-            message,
-        });
-    }
+  constructor(callname = undefined, specialUser = false){
+    this.callname = callname;
+    this.specialUser = specialUser;
+  }
+  
+  messagePage(res, message){
+    res.render("message_page", {
+      callname: this.callname,
+      specialUser: this.specialUser,
+      message,
+    });
+  }
 
-    account(res){
-        res.render("account", {
-            callname: this.callname,
-            specialUser: this.specialUser,
-        });
-    }
+  account(res){
+    res.render("account", {
+      callname: this.callname,
+      specialUser: this.specialUser,
+    });
+  }
 
-    login(res, failMsg = undefined){
-        res.render("login", {
-            callname: this.callname,
-            specialUser: this.specialUser,
-            failMsg,
-        });
-    }
+  login(res, failMsg = undefined){
+    res.render("login", {
+      callname: this.callname,
+      specialUser: this.specialUser,
+      failMsg,
+    });
+  }
 
-    signup(res, failMsg = undefined){
-        res.render("signup", {
-            callname: this.callname,
-            specialUser: this.specialUser,
-            failMsg,
-        });
-    }
+  signup(res, failMsg = undefined){
+    res.render("signup", {
+      callname: this.callname,
+      specialUser: this.specialUser,
+      failMsg,
+    });
+  }
 
-    render(res, pageName){
-        res.render(pageName, {
-            callname: this.callname,
-            specialUser: this.specialUser,
-        });
-    }
+  render(res, pageName){
+    res.render(pageName, {
+      callname: this.callname,
+      specialUser: this.specialUser,
+    });
+  }
 }
 
 module.exports = {
-    Renderer,
+  Renderer,
 };
