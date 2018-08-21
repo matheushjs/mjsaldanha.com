cc66c15fbdeb04e77d3c17092ba5683b44627002
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-08-21 12:42:10 -0300

    Change myip to work with files directly

--- server/model/db_myip.js
+++ server/model/db_myip.js
@@ -1,17 +1,38 @@
 /* db_myip.js
- * Provides wrappers to reading/writing the current IP stored in the database.
+ * Provides wrappers to reading/writing the current IP stored persistently.
  */
 
-const client = require("./db_client").client;
+const fs = require("fs");
+const filePath = "server/db/ip.dat";
 
 // Returns a Promise resolved to the current stored IP (as a string)
 function get(){
-  return client.query("SELECT ip FROM myip WHERE id = 1").then((res) => { return res.rows[0].ip; });
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, "utf8", (err, data) => {
+      if(err){
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
 }
 
 // Changes current stored IP for the given "ip"
 function insert(ip){
-  return client.query("UPDATE myip SET ip = $1 WHERE id = 1", [ip]);
+  return new Promise((resolve, reject) => {
+    fs.writeFile(filePath, ip, "utf8", err => {
+      if(err){
+        reject(err);
+      } else {
+        if(ip.length > 100){
+          reject("Given IP is too big. Possible injection attack detected.");
+        } else {
+          resolve(ip);
+        }
+      }
+    });
+  });
 }
 
 module.exports = {
