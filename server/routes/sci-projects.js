2331fc9505a3ade63f171f24264a13d8340ed177
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-09-25 13:17:20 -0300

    Add page for sci-projects

--- server/routes/sci-projects.js
+++ server/routes/sci-projects.js
@@ -1,8 +1,7 @@
 var express = require("express");
 var router = new express.Router();
 
-router.get("/1-psp-project-1/",
-  (req, res) => req.renderer.render(res, "sci-projects/1-psp-project-1/index")
-);
+router.get("/",                 (req, res) => req.renderer.render(res, "sci-projects/index"));
+router.get("/1-psp-project-1/", (req, res) => req.renderer.render(res, "sci-projects/1-psp-project-1/index"));
 
 module.exports = router;
