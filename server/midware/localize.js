933f038f4eb770b55acf1bd84348d7023a4f0e80
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-11-24 01:01:29 -0200

    Add language switch to the navbar
    	- Fixed precedence of language decision rules in localize.js

--- server/midware/localize.js
+++ server/midware/localize.js
@@ -2,34 +2,38 @@
 /* Sets up the language in which to serve the website.
  * We don't use just cookies because of google crawlers.
  * So the rules are:
- *   1) If we have session.lang, we use it to determine the language
- *   2) If the URL is something like 'mjsaldanha.com/jp/...', we use it to define
+ *   1) If the URL is something like 'mjsaldanha.com/jp/...', we use it to define
  *        the language. Normally, only the index page will be accessed like this.
+ *   2) If we have session.lang, we use it to determine the language
+ *   3) Else, serve english
  * The language will be stored in "req.language", using codes defined in ISO 639-1.
  */
 module.exports = (req, res, next) => {
-  let lang = req.session.language;
+  let urlTokens = req.path.split("/");
+  let langToken = urlTokens[1];     // Language given as directory in the URL
+  let cookieLang = req.session.language;  // Language taken from the cookie
 
-  if(lang){ // If we have the cookie
-    if(lang == "en"){
-      req.language = "en";
-    } else if (lang == "ja"){
-      req.language = "ja";
-    } else {
-      req.language = "en";
-    }
-  } else {  // If we have the language directory in the URL
-    let urlTokens = req.path.split("/");
-    let langToken = urlTokens[1];
-    if(langToken == "ja"){
-      req.language = "ja";
-    } else {
-      req.language = "en";
-    }
+  // Check if we have the language directory in the URL
+  if(langToken === "en"){
+    req.language = "en";
+  } else if(langToken === "ja"){
+    req.language = "ja";
+
+  // Now check language given in cookies
+  } else if (cookieLang === "en"){
+    req.language = "en";
+  } else if (cookieLang === "ja"){
+    req.language = "ja";
+
+  // Default: serve in english
+  } else {
+    req.language = "en";
   }
 
   // We then update the user's cookie
   req.session.language = req.language;
+
+  console.log("Language: " + req.language);
 
   next();
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,6 +5,7 @@ var dbMyip = require("../model/db_myip");
 // Serve home page in multiple languages for Google Crawlers (the Renderer takes care of serving correct language).
 router.get("/",         async (req, res) => req.renderer.render(res, "index"));
 router.get("/ja",       async (req, res) => req.renderer.render(res, "index"));
+router.get("/en",       async (req, res) => req.renderer.render(res, "index"));
 
 router.get("/index",    async (req, res) => req.renderer.render(res, "index"));
 router.get("/credits",  async (req, res) => req.renderer.render(res, "credits"));
