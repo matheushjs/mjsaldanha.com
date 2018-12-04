8a15f0a077ef2c9a498c6d04178d50d01f657aec
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2018-12-04 15:51:09 -0200

    Reorganize folder structure for articles/

--- server/routes/articles.js
+++ server/routes/articles.js
@@ -2,6 +2,6 @@ var express = require("express");
 var router = new express.Router();
 
 router.get("/",           async (req, res) => req.renderer.render(res, "articles/index"));
-router.get("/1-hpc-sspi", async (req, res) => req.renderer.render(res, "articles/1-hpc-sspi"));
+router.get("/1-hpc-sspi", async (req, res) => req.renderer.render(res, "articles/1-hpc-sspi/index"));
 
 module.exports = router;
