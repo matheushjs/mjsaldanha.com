1562c10ddc26dafeb06e637b129374fc441fe543
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2019-04-04 01:06:22 -0300

    Organize validation on signup

--- public/scripts/loginForms.js
+++ public/scripts/loginForms.js
@@ -1,10 +1,29 @@
 function setFailMessage(str){
-  $("form .fail-msg").text(str);
+  var msg = $("form .fail-msg");
+
+  msg.text(msg.text() + " " + str);
   $("form .alert").css("display", "");
 }
 
 function clearErrors(){
+  $("form .fail-msg").text("");
+  $("form .alert").css("display", "none");
   $("form input").removeClass("is-invalid").removeClass("is-valid");
+}
+
+function validateCallname(){
+  var elem = $("form input[name='callname']");
+  var callname = elem.val().trim();
+
+  // For username, we ensure it doesn't have t/l whitespace and has 1-128 characters
+  if(callname.length <= 0 || callname.length > 128){
+    setFailMessage("Name must have at least 1 and at most 128 characters.");
+    elem.addClass("is-invalid");
+    return false;
+  }
+
+  elem.addClass("is-valid");
+  return true;
 }
 
 function validateUsername(){
@@ -61,6 +80,37 @@ function validateLogin(){
   }
 }
 
-window.onload = function(){
-  $("form input[name='username']").focus();
+function validatePassword2(){
+  var elem1 = $("form input[name='password']");
+  var elem2 = $("form input[name='password2']");
+
+  if(elem2.val() !== elem1.val()){
+    setFailMessage("Passwords are not equal.");
+    elem.addClass("is-invalid");
+    return false;
+  }
+
+  elem2.addClass("is-valid");
+  return true;
 }
+
+function validateSignup(){
+  clearErrors();
+
+  var failures = 0;
+  
+  failures += !validateCallname();
+  failures += !validateUsername();
+  failures += !validatePassword();
+  failures += !validatePassword2();
+
+  if(failures == 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+$(document).ready(function(){
+  $("form input").first().focus();
+});
