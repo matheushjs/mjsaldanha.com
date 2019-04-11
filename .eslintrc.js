982cb9127a16f719d98984430e512017b782b5e8
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2019-04-11 19:18:56 -0300

    Add ESLINT

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+var ESL_ENV = process.env.ESL_ENV;
+
+
+if(ESL_ENV === "server") {
+  var env = {
+    "node": true,
+    "es6": true
+  };
+  var ecmaVersion = 2017;
+} else if(ESL_ENV === "browser") {
+  var env = {
+    "browser": true,
+    "commonjs": true,
+    "jquery": true
+  };
+  var ecmaVersion = 5;
+}
+
+module.exports = {
+  "env": env,
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": ecmaVersion,
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "double"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
