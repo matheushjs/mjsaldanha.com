f05b312084ef3c0fb57cfeb73616b7e17b8d383e
Matheus H. J. Saldanha <mhjsaldanha@gmail.com>
2019-05-11 00:10:35 -0300

    add elfnihongo schema

---
+++ server/model/schema_elfnihongo.sql
@@ -0,0 +1,26 @@
+CREATE TABLE topics( /* News Politics, News Society, Runescape, Shinsekai Yori etc */
+	shortdesc CHAR(50) PRIMARY KEY,
+	longdesc TEXT NOT NULL DEFAULT "",
+	url TEXT NOT NULL DEFAULT ""
+);
+
+CREATE TABLE textgenres( /* Casual conversations, news, etc */
+	name CHAR(50) PRIMARY KEY,
+	description TEXT NOT NULL DEFAULT ""
+);
+
+CREATE TABLE paragraphs(
+	id INTEGER PRIMARY KEY,
+	topic CHAR(50) NOT NULL, /* What is the topic related to the paragraph? */
+	textgenre CHAR(50) NOT NULL, /* What is the textual genre? Casual conversation? News excerpt? */
+	content TEXT NOT NULL DEFAULT "",
+	takenFrom TEXT NOT NULL DEFAULT "", /* Where was the excerpt taken from? */
+	CONSTRAINT FK_TOPIC
+		FOREIGN KEY(topic)
+		REFERENCES topics
+		ON DELETE CASCADE,
+	CONSTRAINT FK_GENRE
+		FOREIGN KEY(textgenre)
+		REFERENCES textgenres
+		ON DELETE CASCADE
+);
